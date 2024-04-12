003cfbb18d45b70665a0db71a608e3e7c7769822
Cole <cole.patter@gmail.com>
2024-04-12 16:53:07 -0500

    Update openWeather.js - change http to https in requests

--- assets/js/openWeather.js
+++ assets/js/openWeather.js
@@ -1,14 +1,14 @@
 const apiKey = "4dd0597ea0ca2343fa8a94a80cd7ba6f"
 
 async function getCityByName(query) {
-    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}&units=imperial`)
+    const res = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}&units=imperial`)
     if (!res.ok) throw new Error('Response not OK')
     const data = await res.json()
     return data[0]
 }
 
 async function getForecast(lat, lon) {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
     const res = await fetch(url)
     if (!res.ok) throw new Error('Response not OK')
     const data = await res.json()
